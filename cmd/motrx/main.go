ed0e776af8044c44cb5493a0e0f57d6dfc7b8d76
Phantomvv1 <krisi_andreev@abv.bg>
2026-08-20 16:53:45 +0300

    Made the basic cli and config functionality

--- cmd/motrx/main.go
+++ cmd/motrx/main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import (
+	"log"
+	"os"
 
+	"github.com/Phantomvv1/motrx/internal/cli"
+	"github.com/Phantomvv1/motrx/internal/config"
+)
+
+func main() {
+	filePath, err := cli.StartCli(os.Args)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	config.ParseConfig(filePath)
 }
--- internal/config/config.go
+++ internal/config/config.go
@@ -1,4 +1,14 @@
 package config
 
+import "log"
+
 func ParseConfig(path string) {
+	fileName := ""
+	for i := len(path) - 1; i > 0; i-- {
+		if path[i] == '/' {
+			fileName = path[i+1:]
+		}
+	}
+
+	log.Println(fileName)
 }
